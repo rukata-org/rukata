c72ed9e81370812f3df2c6c92b588f7a6d2d5de6
EngineerN <contact@engineern.dev>
2024-02-26 19:06:05 -0600

    feat: create basic settings subcommand
    Populate the functions for the settings subcommand.
    
    Issue: #1

--- crates/rukata/src/commands/settings.rs
+++ crates/rukata/src/commands/settings.rs
@@ -1,5 +1,6 @@
 use crate::argument_builder::SettingsArguments;
 use crate::command::Command;
+use crate::common::{print_cyan_title, print_white};
 use rukata_settings::SettingsHandler;
 
 pub struct SettingsCommand {
@@ -13,12 +14,41 @@ impl Command for SettingsCommand {
         self.settings = Some(settings)
     }
 
-    fn initialize(&mut self) {
-        todo!()
-    }
+    fn initialize(&mut self) {}
 
     fn execute(&mut self) {
-        todo!()
+        let settings_handler = self.settings.as_mut().expect("Failed to set settings");
+
+        // Print current settings path.
+        print_cyan_title("Current path for settings file:");
+        print_white(settings_handler.get_path());
+
+        // Get the settings
+        let settings = settings_handler.get_mut_settings();
+
+        // Print current settings.
+        print_cyan_title("Current settings data:");
+        print_white(format!("{}", settings));
+
+        let mut has_changed = false;
+
+        // Update the directory if provided.
+        if let Some(directory) = &self.arguments.directory {
+            has_changed = true;
+            settings.set_directory(directory);
+        }
+
+        // If the settings have changed show the user and update file.
+        if has_changed {
+            // Print current settings.
+            print_cyan_title("New settings data:");
+            print_white(format!("{}", settings));
+
+            // Save the settings.
+            if let Err(e) = settings_handler.save() {
+                self.errors.push(e);
+            }
+        }
     }
 
     fn get_errors(&self) -> Vec<String> {

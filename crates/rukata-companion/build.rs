d96b365a9403bfdecdfd199eaac93036a5efbb10
EngineerN <contact@engineern.dev>
2024-03-04 09:08:40 -0600

    refactor: change companion to use mdBook
    In order to keep the environment in the same ecosystem switch to use mdBook for the companion.
    
    mdBook has some limitations, but would make the list of install items short. Making the barrier to entry shorter.
    
    Issue: #1

--- crates/rukata-companion/build.rs
+++ crates/rukata-companion/build.rs
@@ -1,14 +1,10 @@
 use camino::Utf8Path;
-use std::process::Command;
+use mdbook::MDBook;
 use std::{env, fs};
 
-#[cfg(windows)]
-static NPM_CMD: &str = "npm.cmd";
-#[cfg(not(windows))]
-static NPM_CMD: &str = "npm";
-
-fn generate_markdown_files(path: &Utf8Path) {
-    let output_path = path.join("src").join("pages").join("puzzles");
+fn generate_markdown_files(companion_path: &Utf8Path) {
+    let src_path = companion_path.join("src");
+    let output_path = src_path.join("puzzles");
 
     if output_path.exists() {
         if let Ok(directory_entries) = output_path.read_dir_utf8() {
@@ -16,11 +12,11 @@ fn generate_markdown_files(path: &Utf8Path) {
                 let path = entry.path();
                 if path.is_dir() {
                     fs::remove_dir_all(path).unwrap_or_else(|e| {
-                        panic!("Failed to delete `{}` with error `{}`", path, e)
+                        panic!("Failed to delete `{}` with error: {}", path, e)
                     });
                 } else {
                     fs::remove_file(path).unwrap_or_else(|e| {
-                        panic!("Failed to delete `{}` with error `{}`", path, e)
+                        panic!("Failed to delete `{}` with error: {}", path, e)
                     });
                 }
             }
@@ -28,11 +24,13 @@ fn generate_markdown_files(path: &Utf8Path) {
     } else {
         fs::create_dir_all(&output_path).unwrap_or_else(|e| {
             panic!(
-                "Failed to create directory `{}` with error `{}`",
+                "Failed to create directory `{}` with error: {}",
                 output_path, e
             )
         });
     }
+
+    let mut index_puzzle_list = Vec::new();
 
     for key in rukata_puzzle_data::get_id_list() {
         let puzzle_data = rukata_puzzle_data::get_file_data(*key).unwrap();
@@ -42,79 +40,86 @@ fn generate_markdown_files(path: &Utf8Path) {
 
         let puzzle_pid = format!("p{:0>5}", puzzle_id);
 
-        let hyphenated_title = puzzle_pid
-            + "-"
-            + puzzle_title
-                .to_lowercase()
-                .replace(['_', ' '], "-")
-                .as_str()
-            + ".md";
+        let puzzle_path = output_path.join(&puzzle_pid);
+        fs::create_dir_all(&puzzle_path).unwrap_or_else(|e| {
+            panic!(
+                "Failed to create directory `{}` with error: {}",
+                puzzle_path, e
+            )
+        });
+
+        index_puzzle_list.push((puzzle_pid.clone(), puzzle_title.to_string()));
+
         {
-            let readme_file_path = output_path.join(hyphenated_title);
-            let mut readme_string = puzzle_data.get_readme_str().to_string();
-            readme_string =
-                format!("---\ntitle: {:?}\n---\n", puzzle_title) + readme_string.as_str();
+            let readme_file_path = puzzle_path.join("README.md");
+            let readme_string = puzzle_data.get_readme_str();
             fs::write(readme_file_path.clone(), readme_string).unwrap_or_else(|e| {
                 panic!(
-                    "Failed to write file `{}` with error `{}`",
+                    "Failed to write file `{}` with error: {}",
                     readme_file_path, e
                 )
             });
         }
 
         for file in puzzle_readme_files {
-            let file_path = output_path.join(file.get_relative_path());
+            let file_path = puzzle_path.join(file.get_relative_path());
             fs::write(file_path.clone(), file.get_raw_data()).unwrap_or_else(|e| {
-                panic!("Failed to write file `{}` with error `{}`", file_path, e)
+                panic!("Failed to write file `{}` with error: {}", file_path, e)
             });
         }
     }
+
+    let summary_base_path = src_path.join("SUMMARY_template.md");
+    let mut index_content = fs::read_to_string(&summary_base_path).unwrap_or_else(|e| {
+        panic!(
+            "Failed to read file `{}` with error: {}",
+            summary_base_path, e
+        )
+    });
+    index_content.push_str("\n- [Puzzle List]()\n");
+    for item in &index_puzzle_list {
+        index_content.push_str(
+            format!(
+                "   - [{1} - {0}](./puzzles/{1}/README.md)\n",
+                item.1, item.0
+            )
+            .as_str(),
+        );
+    }
+    let index_file_path = src_path.join("SUMMARY.md");
+    fs::write(&index_file_path, index_content).unwrap_or_else(|e| {
+        panic!(
+            "Failed to write file `{}` with error: {}",
+            index_file_path, e
+        )
+    });
 }
 
-fn install_npm_dependencies(path: &Utf8Path) {
-    println!("Installing npm dependencies for libraries/web");
-    let npm_exit_status = Command::new(NPM_CMD)
-        .args(["install"])
-        .current_dir(path)
-        .status()
-        .unwrap();
+fn build_companion(companion_path: &Utf8Path) {
+    let md = MDBook::load(companion_path).unwrap_or_else(|e| {
+        panic!(
+            "Failed to load MDBook companion `{}` with error: {}",
+            companion_path, e
+        )
+    });
 
-    if !npm_exit_status.success() {
-        panic!("Failed to install npm dependencies for libraries/web")
-    }
-}
-
-fn build_npm(path: &Utf8Path) {
-    println!("Building npm project for libraries/lib-web.");
-    let npm_exit_status = Command::new(NPM_CMD)
-        .args(["run", "build"])
-        .current_dir(path)
-        .status()
-        .unwrap();
-
-    if !npm_exit_status.success() {
-        panic!("Failed to build npm portion of libraries/lib-web")
-    }
+    md.build().unwrap_or_else(|e| {
+        panic!(
+            "Failed to build MDBook companion `{}` with error: {}",
+            companion_path, e
+        )
+    });
 }
 
 fn main() {
-    println!("cargo:rerun-if-changed=public");
-    println!("cargo:rerun-if-changed=src/evn.d.ts");
-    println!("cargo:rerun-if-changed=src/pages/index.astro");
-    println!("cargo:rerun-if-changed=src/layouts");
-    println!("cargo:rerun-if-changed=src/styles");
-    println!("cargo:rerun-if-changed=astro.config.mjs");
-    println!("cargo:rerun-if-changed=package.json");
-    println!("cargo:rerun-if-changed=tsconfig.json");
+    // Specify the files to cause rebuilds.
+    println!("cargo:rerun-if-changed=companion");
     println!("cargo:rerun-if-changed=build.rs");
 
     let current_path_string = env::var("CARGO_MANIFEST_DIR").unwrap();
     let current_path = Utf8Path::new(&current_path_string);
+    let companion_path = current_path.join("companion");
 
-    generate_markdown_files(current_path);
-
-    if env::var("SKIP_NPM_BUILD").unwrap_or_default() != "true" {
-        install_npm_dependencies(current_path);
-        build_npm(current_path);
-    }
+    generate_markdown_files(&companion_path);
+    build_companion(&companion_path);
 }
